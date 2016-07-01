676419887bb62cb57413426d616c5569d490d5a5
Alexandre de Souza <xadee.13@gmail.com>
2016-07-01 00:30:27 -0300

    Init project

---
+++ PagarMe.Net.PoCs/PoCFactory.cs
@@ -0,0 +1,26 @@
+﻿using PagarMePoCs.Net.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagarMePoCs.Net
+{
+    public static class PoCFactory
+    {
+        public static IPoC Construct(String PoCName)
+        {
+            IPoC objPoc = null;
+
+            Assembly a = Assembly.GetExecutingAssembly();
+            string typeName = a.GetName().Name + ".Entities." + PoCName;
+            Type frm = a.GetType(typeName);
+
+            objPoc = (IPoC) a.CreateInstance(typeName, true);
+
+            return objPoc;
+        }
+    }
+}
